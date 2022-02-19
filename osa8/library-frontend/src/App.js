efb7e529bcb49674af634475129a09b7dd82c5f8
Jenni Mäkinen <jenni.makinen70@gmail.com>
2022-02-19 22:07:57 +0200

    8.20.-8.22.

--- osa8/library-frontend/src/App.js
+++ osa8/library-frontend/src/App.js
@@ -5,6 +5,7 @@ import NewBook from './components/NewBook'
 import LoginForm from './components/LoginForm'
 import { useApolloClient, useQuery } from '@apollo/client'
 import { ALL_BOOKS } from './queries'
+import Recommend from './components/Recommend'
 
 const App = () => {
   
@@ -34,6 +35,7 @@ const App = () => {
           ? <button onClick={() => setPage('login')}>login</button>
           : <span>
               <button onClick={() => setPage('add')}>add book</button>
+              <button onClick={() => setPage('recommend')}>recommend</button>
               <button onClick={() => logout()}>logout</button>
             </span>
         }
@@ -57,6 +59,11 @@ const App = () => {
         show={page === 'login'}
         setToken={setToken}
         setPage={setPage}
+      />
+
+      <Recommend
+        show={page === 'recommend'}
+        books={books}
       />
 
     </div>
---
+++ osa8/library-frontend/src/components/Recommend.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState} from 'react'
+import { useLazyQuery, useQuery } from '@apollo/client'
+import { ME, ALL_BOOKS } from '../queries'
+
+const Recommend = ({ show, books }) => {
+    const meResponse = useQuery(ME)
+    const [ me, setMe ] = useState(null)
+    const [ recommended, setRecommended ] = useState([])
+    const [ getBooks, result ] = useLazyQuery(ALL_BOOKS)
+
+    useEffect(() => {
+        if(meResponse.data) {
+            setMe(meResponse.data.me)
+            getBooks({ variables: { genre: meResponse.data.me.favoriteGenre } })
+        }
+    }, [meResponse.data, books.data]) // eslint-disable-line
+
+    useEffect(() => {
+        if(result.data) {
+            setRecommended(result.data.allBooks)
+        }
+    }, [result.data, me])
+
+    if(!show) {
+        return null
+    }
+
+    if(meResponse.loading) {
+        return <div>loading ...</div>
+    }
+
+    return (
+        <div>
+            <h2>Recommendations</h2>
+            <p>Books in your favorite genre <strong>{me.favoriteGenre}</strong></p>
+            <table>
+                <tbody>
+                    <tr>
+                        <th></th>
+                        <th>author</th>
+                        <th>published</th>
+                    </tr>
+                    {recommended.map(book => 
+                        <tr key={book.id}>
+                        <td>{book.title}</td>
+                        <td>{book.author.name}</td>
+                        <td>{book.published}</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default Recommend
